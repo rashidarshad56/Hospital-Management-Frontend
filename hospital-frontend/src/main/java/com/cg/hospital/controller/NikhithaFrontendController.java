2a0b3a3f9e96c2bb2a7ebff1cdec12661242db36
Nikhitha-Yerra11 <nikhitha-siva.durga-yerra@capgemini.com>
2025-11-10 09:20:51 +0530

    Update NikhithaFrontendController.java

--- hospital-frontend/src/main/java/com/cg/hospital/controller/NikhithaFrontendController.java
+++ hospital-frontend/src/main/java/com/cg/hospital/controller/NikhithaFrontendController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import java.util.Arrays; 
 import java.util.List;
 
 @Controller
@@ -52,4 +52,5 @@ public class NikhithaFrontendController {
         model.addAttribute("employeeId", employeeId);
         return "appointment";
     }
+
 }
